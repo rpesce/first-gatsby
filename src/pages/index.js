646c40b557fb761ec28df3b1305099f4a6542c65
Roberto <robertopescee@hotmail.com>
2019-05-09 23:25:52 +0200

    add input field component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Hello world! Test</div>
+export default () => (
+	<div style={{ color: `purple` }}>
+		<form>
+			<label>
+				<input type="text" name="url" />
+			</label>
+			<input type="submit" value="Submit" />
+		</form>
+	</div>
+  )
